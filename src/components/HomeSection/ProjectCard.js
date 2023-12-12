5d4bd0091be6a115daadc806e4269eef6d755b1f
Karthikeya Kayithoju <kayithojukarthikeya@gmail.com>
2023-12-12 22:50:12 +0530

    deleted some files

--- src/components/HomeSection/ProjectCard.js
+++ src/components/HomeSection/ProjectCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../pages/index.css';
-// import ProjectDetails from '../ProjectOverview';
 
 const ProjectCard = (props) => {
 
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,17 +2,12 @@ import React, { useEffect } from 'react'
 import Home from './HomeSection/Home'
 import Expertise from './HomeSection/Expertise'
 import Projects from './HomeSection/Projects'
-// import Experience from './HomeSection/Experience'
-// import Footer from './Footer'
 
 const MainContent = () => {
     useEffect(() => {
-        // Check if there's a hash in the URL
         if (window.location.hash) {
             const sectionId = window.location.hash.substring(1); // Remove the '#' character
             const section = document.getElementById(sectionId);
-
-            // Check if the section element exists, and scroll to it
             if (section) {
                 section.scrollIntoView({ behavior: 'smooth' });
             }
